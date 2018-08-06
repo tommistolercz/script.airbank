8f4d5d9d92448ce65e6bb00cd09521f654c4b28e
Tomas Mistoler <tomas.mistoler@gmail.com>
2018-08-07 01:04:28 +0200

    addon's minimal structure and routing updated

--- resources/lib/addon.py
+++ resources/lib/addon.py
@@ -1,8 +1,12 @@
-from xbmcaddon import Addon
-from xbmcplugin import getSetting, setContent, addDirectoryItems, addSortMethod, endOfDirectory, SORT_METHOD_NONE
-from xbmcgui import ListItem
 from sys import argv
 from urlparse import parse_qs
+from xbmcaddon import Addon
+from xbmcgui import ListItem
+from xbmcplugin import setContent, addDirectoryItems, endOfDirectory, addSortMethod, SORT_METHOD_NONE
+
+# debug
+# addon.xml: <import addon="script.module.web-pdb"/>
+# import web_pdb; web_pdb.set_trace()
 
 
 class AirBankPlugin(object):
@@ -10,33 +14,42 @@ class AirBankPlugin(object):
     # plugin initialization
     def __init__(self):
         self.addon = Addon()
-        self.addonHandle = int(argv[1])
-        self.urlBase = argv[0]  # 'plugin://script.airbank/'
-        self.urlQuery = argv[2]  # '?foo=bar' (or empty)
-        self.urlArgs = {}
-        if self.urlQuery.startswith('?'):
-            self.urlArgs = parse_qs(self.urlQuery.lstrip('?'))  # {'foo': ['bar']}
+        self.handle = int(argv[1])
+        self.url = argv[0]
+        self.base = 'plugin://script.airbank'
+        self.path = self.url.replace(self.base, '')
+        self.query = argv[2]
+        self.args = {}
+        if self.query.startswith('?'):
+            self.args = parse_qs(self.query.lstrip('?'))
         self.userSettings = {
-            'contentType': getSetting(self.addonHandle, 'contentType'),
+            'contentType': self.addon.getSetting('contentType'),
+            'itemsPerPage': int(self.addon.getSetting('itemsPerPage')),
+            'apiKey': self.addon.getSetting('apiKey'),
         }
-        setContent(self.addonHandle, self.userSettings['contentType'])
+        setContent(self.handle, self.userSettings['contentType'])
         self.dispatch()
 
     # plugin routing
     def dispatch(self):
-        if (not self.urlArgs) or ('cwd' not in list(self.urlArgs)):
-            self.dirRoot()
-        else:
-            pass
+        if self.path == '/':
+            self.listRoot()
+        elif self.path == '/branches':
+            self.listBranches()
 
     # plugin directory: /
-    def dirRoot(self):
+    def listRoot(self):
+        isFolder = True
         listItems = [
-            ('{0}'.format(self.urlBase), ListItem('HELLO AIRBANK'), False),  # (item url, ListItem, isFolder) TODO: strings.po
+            (self.base + '/branches', ListItem(self.addon.getLocalizedString(30004)), isFolder),
         ]
-        addDirectoryItems(self.addonHandle, listItems, len(listItems))
-        addSortMethod(self.addonHandle, SORT_METHOD_NONE)
-        endOfDirectory(self.addonHandle, updateListing=True, cacheToDisc=True)
+        addDirectoryItems(self.handle, listItems, len(listItems))
+        addSortMethod(self.handle, SORT_METHOD_NONE)
+        endOfDirectory(self.handle)
+
+    # plugin directory: /branches
+    def listBranches(self):
+        pass
 
 
 # and action! ;-)
